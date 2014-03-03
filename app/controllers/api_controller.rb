a226efaceb90df0c4811fcfd1c1ce24c48ce1ff7
Tiffany Dharma <tdharma@stanford.edu>
2014-03-03 15:49:07 -0800

    Added line chart page/nvd3 code - needs backend functionality

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -54,6 +54,112 @@ class ApiController < ApplicationController
 
 	end
 
+	# copy of def bar to show line
+	def line
+		# Retrieve the data set
+		id = params[:id]
+		@dataset = Dataset.where(:base_url => id).first
+
+		# POST requests are typically associated with some chart
+ 		if request.post?
+
+ 			# Figure out what sort of chart it is
+ 			chart = params[:chart]
+
+ 			# Handles line chart data requests
+ 			if chart == 'line'
+
+				# Get the key and the aggregate
+				key = params[:key]
+				aggregate = params[:aggregate]
+
+	 			# Render as JSON data
+	 			render json: aggregate_data(key, aggregate)
+
+	 		# Handles bar chart data requests
+	 		elsif chart == 'line'
+
+	 			# Get the key and aggregate
+	 			key = params[:key]
+	 			aggregate = params[:aggregate]
+
+	 			# Put in the format expected of bar charts
+	 			line_data = Hash.new
+	 			line_data["key"] = key
+	 			line_data["values"] = aggregate_data(key, aggregate)
+
+	 			# Render as JSON data
+	 			json_data = Array.new
+	 			json_data.push(line_data)
+	 			render json: json_data
+
+	 		# If we don't receive a chart type, handle as a filtered data request
+	 		else
+
+	 			# Get the filters from the parameter
+	 			filters = params[:filters]
+
+	 			# Start construction our query here
+	 			query = @dataset.datadocs
+
+	 			# Construct a query with criteria
+	 			criteria = Criteria.new
+
+				# Keep track of equals filters
+				equals_filters = {}
+
+				# Get a pointer to the attributes from the dataset
+				attrs = JSON.parse(@dataset.attrs)
+
+	 			# Iterate through the objects in the filter to build the query
+	 			filters.each do |filter|
+
+	 				# When we build the query, if we have multiple things fulfilling
+	 				# the same pattern (eg. two attributes that are both equals) the
+	 				# default is to apply an OR clause on the two statements. There
+	 				# is currently no override for this. This should be the behavior
+	 				# that is expected by the user.
+
+	 				# Pull the correct values out from the filter
+	 				filter_attribute = filter[1]["attribute"]
+	 				filter_sign = filter[1]["sign"]
+	 				filter_value = filter[1]["value"]
+
+	 				# Cast for numerics
+	 				if attrs[filter_attribute] == 'Numeric'
+	 					filter_value = filter_value.to_f
+	 				end
+
+ 					# Start building our query filter
+ 					if filter_sign == "="
+		 				if not equals_filters.include?(filter_attribute)
+	 						equals_filters[filter_attribute] = Array.new()
+	 					end
+	 	 				equals_filters[filter_attribute].push(filter_value)
+	 				elsif filter_sign == "<"
+ 						query = query.lt(:"#{filter_attribute}" => filter_value)
+ 					elsif filter_sign == "<="
+ 						query = query.lte(:"#{filter_attribute}" => filter_value)
+ 					elsif filter_sign == ">"
+ 						query = query.gt(:"#{filter_attribute}" => filter_value)
+ 					elsif filter_sign == ">="
+ 						query = query.gte(:"#{filter_attribute}" => filter_value)
+ 					end
+
+	 			end
+
+	 			# Assemble everything with equal signs
+	 			equals_filters.each do |key, value|
+	 				query = query.in(:"#{key}" => value)
+	 			end
+
+	 			# Render the result as JSON
+	 			render json: query
+
+	 		end
+
+	 	end
+	end
 
 	def bar
 		# Retrieve the data set
