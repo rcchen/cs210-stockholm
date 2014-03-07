42a14830f1074f70be545fa52599421d2ede3c80
Roger Chen <rogerthechen@gmail.com>
2014-03-07 14:47:54 -0800

    Getting things set up for saving visualizations

--- app/controllers/visualization_controller.rb
+++ app/controllers/visualization_controller.rb
@@ -7,6 +7,33 @@ class VisualizationController < ApplicationController
 
 	end
 
+	def save
+
+		# Create a new Visualization object
+		visualization = Visualization.new
+
+		# Now set the various things to our POST params
+		visualization.dataset = params[:dataset]
+		visualization.filters = params[:filters]
+		visualization.chart_type = params[:chart_type]
+		visualization.chart_options = params[:chart_options]
+		visualization.identifier = params[:identifier]
+
+		# Find the corresponding worksheet
+		worksheet_identifier = params[:w]
+		worksheet = Worksheet.get_by_identifier(worksheet_identifier)
+
+		# Save the visualization to the worksheet
+		worksheet.visualizations << visualization
+
+		# Save the worksheet
+		worksheet.save
+
+		# Redirect to the worksheet page
+		redirect_to '/worksheet/' + worksheet_identifier
+
+	end
+
 	def edit
 
 		# Retrieve the correct visualization
