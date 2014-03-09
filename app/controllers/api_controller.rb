46034049d02c0ac62daa32ec71dab9171d0d1d2c
Eri Gamo <gamoeri@gmail.com>
2014-03-09 16:50:31 -0700

    pie chart combine small data isn't working yet, look in api controller, aggregate_data method

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -7,14 +7,15 @@ class ApiController < ApplicationController
 	end
 
 	# Route for data aggregation
-	def aggregate_data(key, aggregate)
+	def aggregate_data(key, aggregate, combine)
 
 		# We're essentially doing a map/reduce operation
 		# We collect all the keys in the data hash and 
 		# aggregate values for the keys
+		num_keys = 0
+		total = 0
 		data = Hash.new
 		@results.each do |datadoc|
-			
 			# Get the key
 			key_value = datadoc["#{key}"]
 
@@ -32,6 +33,18 @@ class ApiController < ApplicationController
 			# Add in the value that we observed for the aggregate
 			data[key_value] = data[key_value] + aggregate_value
 
+			#For calculating percentage
+			total = total + aggregate_value
+			num_keys = num_keys + 1
+
+		end
+
+		if combine == 'true'
+			data["other"] = 0
+			data.each do |key, value|
+				
+			end
+			#data['22'] = total
 		end
 
 		# Compile data into format expected of pie charts
@@ -47,59 +60,7 @@ class ApiController < ApplicationController
 		return json_data.sort_by { |hash| hash["label"] }
 
 	end
- # group anything under this percentage (5%)
- #display only top ++ or bottom ___
-	def aggragate_small
-		# We're essentially doing a map/reduce operation
-		# We collect all the keys in the data hash and 
-		# aggregate values for the keys
-		num_keys = 0
-		total = 0
-		data = Hash.new
-		@dataset.datadocs.each do |datadoc|
-			
-			# Get the key
-			key_value = datadoc["#{key}"]
-
-			# By default, we count the aggregate
-			aggregate_value = 1
-			if key != aggregate
-				aggregate_value = datadoc["#{aggregate}"].to_i
-			end
-
-			# If the key does not exist yet, set it to zero
-			if not data.key?(key_value)
-				num_keys = num_keys + 1
-				data[key_value] = 0
-			end
-
-			# Add in the value that we observed for the aggregate
-			total = total + aggregate_value
-			data[key_value] = data[key_value] + aggregate_value
-
-		end
-
-		#go through hash, calculate % and add to other if lower than 5%
-
-		data.each do |key, value|
-
-		end
-
-		# Compile data into format expected of pie charts
-		json_data = Array.new
-		data.each do |key, value|
-			json_data_object = Hash.new
-			if value/total < 0.05
-				json_data_object['label']
-			json_data_object["label"] = key
-			json_data_object["value"] = value
-			json_data << json_data_object
-		end
-
-		# Return the aggregated data
-		return json_data
-	end
-	
+ 
 	def filter_documents(filters)
 		# Start construction our query here
 		query = @dataset.datadocs
@@ -182,15 +143,17 @@ class ApiController < ApplicationController
  			# Figure out what sort of chart it is
  			chart = params[:chart]
 
+			#Combine small data?
+ 			combine = params[:combine] 
+
  			# Handles pie chart data requests
  			if chart == 'pie'
 
 				# Get the key and the aggregate
 				key = params[:key]
 				aggregate = params[:aggregate]
-
 	 			# Render as JSON data
-	 			render json: aggregate_data(key, aggregate)
+	 			render json: aggregate_data(key, aggregate, combine)
 
 	 		# Handles bar chart data requests
 	 		elsif chart == 'bar'
@@ -202,7 +165,7 @@ class ApiController < ApplicationController
 	 			# Put in the format expected of bar charts
 	 			bar_data = Hash.new
 	 			bar_data["key"] = key
-	 			bar_data["values"] = aggregate_data(key, aggregate)
+	 			bar_data["values"] = aggregate_data(key, aggregate, combine)
 
 	 			# Render as JSON data
 	 			json_data = Array.new
