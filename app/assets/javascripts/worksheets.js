c7df1f2757775bd7f3af5f3edd89bc60ee742fed
Roger Chen <rogerthechen@gmail.com>
2014-06-09 09:46:38 -0700

    Temp changes

--- app/assets/javascripts/worksheets.js
+++ app/assets/javascripts/worksheets.js
@@ -21,6 +21,76 @@ function convertSymbolToText(symbol) {
 	}
 }
 
+var saveSelection, restoreSelection;
+
+if (window.getSelection && document.createRange) {
+    saveSelection = function(containerEl) {
+        var range = window.getSelection().getRangeAt(0);
+        var preSelectionRange = range.cloneRange();
+        preSelectionRange.selectNodeContents(containerEl);
+        preSelectionRange.setEnd(range.startContainer, range.startOffset);
+        var start = preSelectionRange.toString().length;
+
+        return {
+            start: start,
+            end: start + range.toString().length
+        }
+    };
+
+    restoreSelection = function(containerEl, savedSel) {
+        var charIndex = 0, range = document.createRange();
+        range.setStart(containerEl, 0);
+        range.collapse(true);
+        var nodeStack = [containerEl], node, foundStart = false, stop = false;
+        
+        while (!stop && (node = nodeStack.pop())) {
+            if (node.nodeType == 3) {
+                var nextCharIndex = charIndex + node.length;
+                if (!foundStart && savedSel.start >= charIndex && savedSel.start <= nextCharIndex) {
+                    range.setStart(node, savedSel.start - charIndex);
+                    foundStart = true;
+                }
+                if (foundStart && savedSel.end >= charIndex && savedSel.end <= nextCharIndex) {
+                    range.setEnd(node, savedSel.end - charIndex);
+                    stop = true;
+                }
+                charIndex = nextCharIndex;
+            } else {
+                var i = node.childNodes.length;
+                while (i--) {
+                    nodeStack.push(node.childNodes[i]);
+                }
+            }
+        }
+
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+} else if (document.selection && document.body.createTextRange) {
+    saveSelection = function(containerEl) {
+        var selectedTextRange = document.selection.createRange();
+        var preSelectionTextRange = document.body.createTextRange();
+        preSelectionTextRange.moveToElementText(containerEl);
+        preSelectionTextRange.setEndPoint("EndToStart", selectedTextRange);
+        var start = preSelectionTextRange.text.length;
+
+        return {
+            start: start,
+            end: start + selectedTextRange.text.length
+        }
+    };
+
+    restoreSelection = function(containerEl, savedSel) {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(containerEl);
+        textRange.collapse(true);
+        textRange.moveEnd("character", savedSel.end);
+        textRange.moveStart("character", savedSel.start);
+        textRange.select();
+    };
+}
+
 var Worksheet = Backbone.Model.extend({
 
 	urlRoot: '/worksheets/',
@@ -386,10 +456,6 @@ var WorksheetView = Backbone.View.extend({
 		'ctrl+s': 			'save',
 		'command+e': 		'justifyCenter',
 		'ctrl+e': 			'justifyCenter',
-		'command+1': 		'insertVisualization', 
-		'ctrl+1': 			'insertVisualization',
-		'command+2': 		'insertImage', 
-		'ctrl+2':  			'insertImage'
 	},
 
 	initialize: function() {
@@ -502,7 +568,10 @@ var WorksheetToolbarView = Backbone.View.extend({
 
 	addImage: function() {
 
-		worksheetView.insertImage();
+		saveSelection(document.getElementById('storylytics-editor'));
+
+		restoreSelection(document.getElementById('storylytics-editor'), 'asssss');
+		//worksheetView.insertImage();
 
 	}
 
