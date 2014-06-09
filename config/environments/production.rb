54de570eb06e20ae42ffe424e9bad60617df0204
Roger Chen <rogerthechen@gmail.com>
2014-06-09 01:49:00 -0700

    Added the mousetrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Cs210Stockholm::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( worksheets.css )
+  config.assets.precompile += %w( worksheets.css, mousetrap.js, mousetrap.plugin.backbone.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
